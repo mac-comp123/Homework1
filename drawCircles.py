2da5083c083b6964a5414e87029ed9993f5a1037
Amin G. Alhashim, PhD, 6σ <amin.g.alhashim@gmail.com>
2024-01-24 21:10:50 -0600

    Add required files

---
+++ drawCircles.py
@@ -0,0 +1,38 @@
+"""
+Draws six circles, centered on the origin, each a different color.
+Assumption: the number of colors in the turtle colors list is at least
+as long as the number of rings.
+"""
+
+
+import turtle
+import math
+
+scr = turtle.Screen()
+turt = turtle.Turtel()
+
+scr.bgcolor("seashell")
+
+tColors = ["light salmon", "light sky blue", "pale green", "liht coral", "pale turquoise", "plum"]
+
+turt.width(5)
+numRings = 6
+
+for i in range(numRings):
+    turt.color(tColors[0])
+    radius = 40 * (i + 1)
+
+    
+    turt.up()
+    turt.forward(radius)
+    turt.down()
+    
+    turt.left(90)
+    turt.circle(i)
+    turt.right(60)
+    
+    turt.up
+    turt.backward(radius)
+    turt.down()
+
+scr.exitonclick()
---
+++ mysteryScript.py
@@ -0,0 +1,7 @@
+name = input("What is your name? ")
+
+print("Hooray for")
+for c in name:
+    print(c)
+    print()
+print("!")
